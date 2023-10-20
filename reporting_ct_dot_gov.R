16c058098b46c4483fc05aba3da31be5e76e9300
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2023-10-20 10:52:27 -0400

    Added start of reporting script

---
+++ reporting_ct_dot_gov.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(ggplot2)
+library(cthist) ## https://github.com/bgcarlisle/cthist
+
+extractions <- read_csv("ctg-studies.csv") %>%
+    rename(nctid = "NCT Number") %>%
+    head(n=25)
+
+if (! file.exists("historical_versions.csv")) {
+    clinicaltrials_gov_download(nctids, "historical_versions.csv")
+}
+
+hv <- read_csv("historical_versions.csv")
+
+hv %>%
+    select(nctid, version_date, results_posted) %>%
+    print(n=100)
+
+hv %>%
+    filter(results_posted) %>%
+    select(nctid, version_date)
+
+hv %>%
+    filter(results_posted) %>%
+    group_by(nctid) %>%
+    slice_head() %>%
+    select(nctid, version_date, primary_completion_date) %>%
+    mutate(diff = version_date - primary_completion_date)
+    
+## use `nrow()` and `distinct()` functions to count how many trials
+## never post results
+
+## use `nrow()` function to count how many DO post results
+
+## use ggplot to make an histogram of the number of days it takes for
+## trials that do post results
